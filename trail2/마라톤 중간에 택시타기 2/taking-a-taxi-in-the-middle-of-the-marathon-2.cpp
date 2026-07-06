2c3d9a04f20804bb6d05912d9c32ce8bac6c3d78
Ruysong <rruysong@gmail.com>
2026-07-07 00:25:59 +0900

    [Passed] 마라톤 중간에 택시타기 2 (2ms, 0MB)

---
+++ trail2/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin>> n;
+    int x[101], y[101];
+
+    for(int i = 0; i < n; i++) {
+    cin >> x[i] >> y[i];
+}   
+    int Minrange = INT_MAX;
+    
+    for(int i=1;i< n-1; i++){
+        int range=0;
+        for(int j=0;j<n-1; j++){
+        range += abs(x[j+1]-x[j])+abs(y[j+1]-y[j]);
+        if ((j) == i) {
+            range -= abs(x[j+1]-x[j])+abs(y[j+1]-y[j]);
+            range -= abs(x[j]-x[j-1])+abs(y[j]-y[j-1]);
+            range += abs(x[j+1]-x[j-1])+abs(y[j+1]-y[j-1]);
+        }
+        }
+        Minrange = min(range,Minrange);
+    }
+    cout << Minrange;
+    return 0;
+}
