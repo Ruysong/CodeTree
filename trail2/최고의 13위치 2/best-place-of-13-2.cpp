4ef3c45527ee5518ce62d91f8b82276d13832750
Ruysong <rruysong@gmail.com>
2026-07-07 22:54:51 +0900

    [Wrong Answer] 최고의 13위치 2 (2ms, 0MB)

--- trail2/최고의 13위치 2/best-place-of-13-2.cpp
+++ trail2/최고의 13위치 2/best-place-of-13-2.cpp
@@ -5,6 +5,33 @@ int n;
 
 int main() {
     cin >> n;
-       
+    int mat[20][20];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>> mat[i][j];
+        }
+    }
+    int ans=0;
+    int cnt =0;
+    int sum[20][20];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-2;j++){
+            sum[i][j]= mat[i][j]+mat[i][j+1]+mat[i][j+2];
+        }
+    }
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n-2;j++){
+             for(int k=0; k<n;k++){
+                for(int l=0;l<n-2;l++){
+                   if(!(i<=k && k<=i+2)) cnt = sum[i][j]+ sum[k][l];
+                    ans = max(cnt,ans);
+                    cnt =0;
+                }
+             }
+            
+        }
+    }
+    cout << ans;
+
     return 0;
 }
