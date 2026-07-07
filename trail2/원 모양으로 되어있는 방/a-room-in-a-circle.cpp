e3c44620c1fe13cfe4f04b3118da89c957d95e7f
Ruysong <rruysong@gmail.com>
2026-07-07 15:11:09 +0900

    [Passed] 원 모양으로 되어있는 방 (8ms, 0MB)

---
+++ trail2/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin>> n;
+    int room[1010];
+    for(int i=0;i<n;i++){
+        cin >> room[i];
+    }
+    int total =0;
+    for(int i=0;i<n;i++){
+        total += room[i];
+    }
+    int range_min = INT_MAX;
+    for(int i=0;i<n;i++){ //gotta decide first room
+        int range=0;
+        int a =total;
+        for(int j=0;j<n;j++){
+            a= a-room[(i+j+n)%n];
+            
+            if(a<0) {
+                break;}
+            range += a;
+        }
+        range_min = min(range_min,range);
+        
+    }
+    cout << range_min;
+    return 0;
+}
