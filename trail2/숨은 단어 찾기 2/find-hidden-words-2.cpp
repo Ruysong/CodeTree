e72486aca3b1fb95ea39ee43a3d4e0835ec8da1d
Ruysong <rruysong@gmail.com>
2026-07-07 16:49:53 +0900

    [Passed] 숨은 단어 찾기 2 (2ms, 0MB)

--- trail2/숨은 단어 찾기 2/find-hidden-words-2.cpp
+++ trail2/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int n,m;
 bool InRange(int x, int y){
     return (x>=0 && x<n)
-    && (y>=0 && y <n);
+    && (y>=0 && y <m);
 }
 int main() {
-    
     cin >> n>>m;
-    int name[50][50];
-    int dx[5]={-1,-1,0,1,1};
-    int dy[5]={0,1,1,1,0};
+    string name[50];
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
     for(int i=0; i<n;i++){
-        for(int j=0;j<m;j++){
-            cin >> name[i][j];
-        }
+        cin >> name[i];
     }
 
     int ans=0;
     for(int i=0; i<n;i++){
         for(int j=0;j<m;j++){
             if(name[i][j] =='L'){
-                for(int k=0;k<5;k++){
+                for(int k=0;k<8;k++){
+                     int cnt =0;
                     for(int l=1;l<=2;l++){
-                    int  cnt =0;
-                    int nx = i + dx[k]*l;
+                   int nx = i + dx[k]*l;
                     int ny = j + dy[k]*l;
-                    if(Inrange(nx,ny && name[nx][ny] =='E') 
-                    &&){cnt++;}
-                    if (cnt ==2) ans++;
-                   
+                    if(InRange(nx,ny) && name[nx][ny] =='E') 
+                    {
+                    cnt++;}
+                 
                 }
+                    if (cnt ==2) ans++;
                 }
 
 
