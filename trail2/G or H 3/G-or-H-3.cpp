b0fb9087398f2acaa1b4edaeb2a854713a3f6549
Ruysong <rruysong@gmail.com>
2026-07-08 15:28:30 +0900

    [Wrong Answer] G or H 3 (2ms, 0MB)

---
+++ trail2/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,k;
+
+int main() {
+    cin >> n >>k;
+    int placed[100];
+    for(int i = 0; i < n; i++){
+       int a; //position
+       char b; 
+       cin >> a>>b;
+       if(b =='G') placed[a] = 1;
+       else if(b='H') placed[a] = 2;
+    }
+    
+    int max_cnt = 0;
+    for(int i = 0; i <= n - k + 1; i++) {
+        int candy_num = 0;
+        for(int j = i; j < i + k; j++)
+            candy_num += placed[j];
+    
+        max_cnt = max(max_cnt, candy_num);
+    }
+    
+    cout << max_cnt;
+    
+    return 0;
+}
+
+
