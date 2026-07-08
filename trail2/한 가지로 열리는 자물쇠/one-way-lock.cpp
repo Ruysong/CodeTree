173cbb10a9254d0b51fdfdd41c87933cd26e534a
Ruysong <rruysong@gmail.com>
2026-07-08 21:58:55 +0900

    [Passed] 한 가지로 열리는 자물쇠 (2ms, 0MB)

--- trail2/한 가지로 열리는 자물쇠/one-way-lock.cpp
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -6,5 +6,16 @@ int main() {
     cin  >> n;
     int a,b,c;
     cin >> a>>b>>c;
+    int cnt=0;
+    for(int i=1;i <=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n ; k++){ // i j k의 조합.
+                if( abs(i -a) <=2 
+                || abs(j -b) <=2 
+                || abs(k -c) <=2   ) cnt++;
+            }
+        }
+    }
+    cout << cnt;
     return 0;
 }
