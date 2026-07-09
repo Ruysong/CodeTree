a8eff7284bbc148c3c9c7d6bade4acac721d466d
Ruysong <rruysong@gmail.com>
2026-07-09 16:44:27 +0900

    [Passed] 이상한 폭탄 2 (2ms, 0MB)

--- trail2/이상한 폭탄 2/strange-bomb-2.cpp
+++ trail2/이상한 폭탄 2/strange-bomb-2.cpp
@@ -11,14 +11,14 @@ int main() {
     cin >> n>>k;
     for(int i = 0; i < n; i++) cin>> pos[i];
         
-    int ans = 0;
-
+    int ans = -1;
    for(int i = 0; i < n; i++) 
         for(int j = i-k; j <= i+k; j++){
-           if( i-k < 0)  continue;
+           if( j < 0)  continue;
+           if( j >= n)  break;
            if( i==j)  continue;
 
-           if(pos[j]==pos[i]) ans = max(j,ans);
+           if(pos[j]==pos[i]) ans = max(pos[j],ans);
            
         }
 
