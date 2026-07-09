82bebb711377995ee578219861b6dda00d2de5e5
Ruysong <rruysong@gmail.com>
2026-07-09 17:03:17 +0900

    [Compile Error] 스승의 은혜 3

---
+++ trail2/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,b;
+    cin >> n>>b;
+    int price[1000][2]={};
+    
+    for(int i=0;i<n;i++) cin >> price[i][0]>> price[i][1] ; //price랑 shipping가격.
+    int ans=0;
+       
+    #include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,b;
+    cin >> n>>b;
+    int price[1000][2]={};
+    
+    for(int i=0;i<n;i++) cin >> price[i][0]>> price[i][1] ; //price랑 shipping가격.
+    int ans=0;
+       
+    for(int i=0;i<n;i++){ //i는 반값
+         int tmp[1000][2] = {};
+        
+        for(int j = 0; j < n; j++){
+        tmp[j][0] = price[j][0];
+        tmp[j][1] = price[j][1];
+        }
+
+        tmp[i][0] /= 2;
+        for(int j = 0; j < n; j++){
+        tmp[j][0]  += tmp[j][1] ;
+        }
+        
+         sort(tmp[0][0], tmp[n-1][0]);
+
+         int money =b;
+         int ppl=0;
+            for(int j=0;j<n;j++){
+            if(tmp[j][0] <= money) {money -= tmp[j][0];
+            ppl++;
+            }
+            else if(tmp[j][0] > money) break;
+
+        }
+        ans = max(ans, ppl);
+        
+    }
+    cout << ans;
+    return 0;
+}
+    cout << ans;
+    return 0;
+}
