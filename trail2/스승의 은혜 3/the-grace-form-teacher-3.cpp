d705b689ef9f237c9611fd735df245230a786e75
Ruysong <rruysong@gmail.com>
2026-07-09 17:04:33 +0900

    [Passed] 스승의 은혜 3 (2ms, 1MB)

--- trail2/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ trail2/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -10,45 +10,33 @@ int main() {
     for(int i=0;i<n;i++) cin >> price[i][0]>> price[i][1] ; //price랑 shipping가격.
     int ans=0;
        
-    #include <iostream>
-#include <algorithm>
-using namespace std;
+  for(int i = 0; i < n; i++) { // i번 학생 선물에 쿠폰 사용
+    int cost[1000];
 
-int main() {
-    int n,b;
-    cin >> n>>b;
-    int price[1000][2]={};
-    
-    for(int i=0;i<n;i++) cin >> price[i][0]>> price[i][1] ; //price랑 shipping가격.
-    int ans=0;
-       
-    for(int i=0;i<n;i++){ //i는 반값
-         int tmp[1000][2] = {};
-        
-        for(int j = 0; j < n; j++){
-        tmp[j][0] = price[j][0];
-        tmp[j][1] = price[j][1];
-        }
-
-        tmp[i][0] /= 2;
-        for(int j = 0; j < n; j++){
-        tmp[j][0]  += tmp[j][1] ;
-        }
-        
-         sort(tmp[0][0], tmp[n-1][0]);
-
-         int money =b;
-         int ppl=0;
-            for(int j=0;j<n;j++){
-            if(tmp[j][0] <= money) {money -= tmp[j][0];
-            ppl++;
-            }
-            else if(tmp[j][0] > money) break;
-
-        }
-        ans = max(ans, ppl);
-        
+    for(int j = 0; j < n; j++) {
+        cost[j] = price[j][0] + price[j][1];
     }
+
+    cost[i] = price[i][0] / 2 + price[i][1];
+
+    sort(cost, cost + n);
+
+    int money = b;
+    int ppl = 0;
+
+    for(int j = 0; j < n; j++) {
+        if(cost[j] <= money) {
+            money -= cost[j];
+            ppl++;
+        }
+        else {
+            break;
+        }
+    }
+
+    ans = max(ans, ppl);
+}
+
     cout << ans;
     return 0;
 }
