55d03e1ea8b65a2a050585bb2fd418d100810af7
Ruysong <rruysong@gmail.com>
2026-07-09 16:23:59 +0900

    [Wrong Answer] 개발자의 순위 (2ms, 0MB)

---
+++ trail2/개발자의 순위/developers-rank.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int k, n;
+int pos[11][21]; 
+// pos[경기번호][개발자번호] = 그 경기에서의 순위
+
+int main() {
+    cin >> k >> n;
+    for(int game = 0; game < k; game++) {
+        for(int rank = 1; rank <= n; rank++) {
+            int dev;
+            cin >> dev;
+
+            pos[game][dev] = rank;
+        }
+    }
+    int ans = 0;
+   for(int i = 0; i < n; i++) 
+        for(int j = i+1; j < n; j++){
+            int cnt =0;
+            
+            for(int game = 0; game < k; game++){
+                if( pos[game][i] <  pos[game][j]) cnt++;
+            }
+            if(cnt ==k) ans++;
+
+        }
+
+    cout << ans;
+
+    return 0;
+}
