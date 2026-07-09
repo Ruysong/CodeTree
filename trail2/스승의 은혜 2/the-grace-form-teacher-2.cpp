54de61b95b571ce567d8b6558fca9c71de83cf1f
Ruysong <rruysong@gmail.com>
2026-07-09 14:24:26 +0900

    [Passed] 스승의 은혜 2 (2ms, 0MB)

--- trail2/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ trail2/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -6,19 +6,20 @@ int main() {
     int n,b;
     cin >> n>>b;
     int price[1000]={};
-    for(int i=0;i<n;i++) cin >> price[i] >> price[i];
+    for(int i=0;i<n;i++) cin >> price[i] ;
     int ans=0;
     sort(price,price+n);
     for(int i=0;i<n;i++){
          price[i] /=2; // 반값을 쓸.
+         
          int money =b;
          int ppl=0;
             for(int j=0;j<n;j++){
-            if(price[j] <money) {money -= price[j];
+            if(price[j] <= money) {money -= price[j];
             ppl++;
             }
             else if(price[j] > money) break;
-              
+
         }
         ans = max(ans, ppl);
         price[i] *=2;
