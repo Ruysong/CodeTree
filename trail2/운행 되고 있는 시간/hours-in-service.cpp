7b2328e29e5e8280e52cb03e62ca5b141e5bb631
Ruysong <rruysong@gmail.com>
2026-07-09 13:27:58 +0900

    [Passed] 운행 되고 있는 시간 (2ms, 0MB)

--- trail2/운행 되고 있는 시간/hours-in-service.cpp
+++ trail2/운행 되고 있는 시간/hours-in-service.cpp
@@ -8,17 +8,19 @@ int main() {
     int time[100][2]={};
     for(int i=0;i<n;i++) cin >> time[i][0] >> time[i][1];
     int ans=0;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){ //뺼놈
+        int fill[1001]= {0};
         for(int j=0;j<n;j++){
             if(i ==j) continue;
-            int fill[1001]= {0};
+            
             for(int k=time[j][0];k<time[j][1];k++ ){
                 fill[k]=1;
             }
-            int op_time=0;
-            for(int k=0;k<1000;k++) op_time += fill[k];
-            ans = max(ans,op_time);
+            
         }
+        int op_time=0;
+        for(int k=0;k<1000;k++) op_time += fill[k];
+        ans = max(ans,op_time);
        
     }
     cout << ans;
