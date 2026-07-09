3993c5a4913d8d64c7baf30e2c32ce0f829b8cc4
Ruysong <rruysong@gmail.com>
2026-07-09 14:22:21 +0900

    [Wrong Answer] 스승의 은혜 2 (2ms, 0MB)

---
+++ trail2/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,b;
+    cin >> n>>b;
+    int price[1000]={};
+    for(int i=0;i<n;i++) cin >> price[i] >> price[i];
+    int ans=0;
+    sort(price,price+n);
+    for(int i=0;i<n;i++){
+         price[i] /=2; // 반값을 쓸.
+         int money =b;
+         int ppl=0;
+            for(int j=0;j<n;j++){
+            if(price[j] <money) {money -= price[j];
+            ppl++;
+            }
+            else if(price[j] > money) break;
+              
+        }
+        ans = max(ans, ppl);
+        price[i] *=2;
+    }
+    cout << ans;
+    return 0;
+}
