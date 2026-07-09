b4efd862e196c3681deeec36908c504619c9988b
Ruysong <rruysong@gmail.com>
2026-07-09 12:49:35 +0900

    [Wrong Answer] 가장 가까운 두 점 사이의 거리 (2ms, 0MB)

---
+++ trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n ;
+int segments[100][2] = {};
+
+int main() {
+    cin >> n;
+    int ans = 100;
+
+    for(int i = 0; i < n; i++)
+         cin >> segments[i][0]>> segments[i][1];
+
+
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j < n; j++) {
+                        
+                int x1 = segments[i][0], y1 = segments[i][1];
+                int x2 = segments[j][0], y2 = segments[j][1];
+                int range = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+                            ans = min(ans, range);
+        }
+    
+    cout << ans;
+    
+    return 0;
+}
+
