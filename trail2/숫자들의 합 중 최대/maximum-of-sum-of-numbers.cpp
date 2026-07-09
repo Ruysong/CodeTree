a523e09a234fcf9087ec361a14359c10f6df8b44
Ruysong <rruysong@gmail.com>
2026-07-10 00:37:32 +0900

    [Wrong Answer] 숫자들의 합 중 최대 (2ms, 0MB)

---
+++ trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x>>y;
+    int ans=0;
+    for(int i=x;i<=y;i++){
+        int a,b,c,d,e;
+        a =i /1000; // 1000의자리 근데 만까지 커버.
+        b= i/100; // 100의자리
+        c =(i/10); //10의자리.
+        d = i %10; //1의자리
+        ans = max(ans, a+b+c+d);
+    }
+    cout << ans;
+    return 0;
+}
