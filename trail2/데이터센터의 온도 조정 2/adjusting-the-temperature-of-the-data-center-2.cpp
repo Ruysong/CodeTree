e92328cd16f92bc97fcf6b8067e22954e1264475
Ruysong <rruysong@gmail.com>
2026-07-10 19:51:11 +0900

    [Wrong Answer] 데이터센터의 온도 조정 2 (4ms, 0MB)

---
+++ trail2/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int max_score = 0;
+int n,c,g,h;
+int temp[1000][2]={};
+
+int Workload(int num, int a,int b) {
+    if(num < a)
+        return c;
+    else if(num < b)
+        return g;
+    else 
+        return h;
+}
+
+int main() {
+    cin >> n>> c>>g>>h;
+    for(int i = 0; i < n; i++) cin >> temp[i][0]>> temp[i][1];
+
+    int ans=0;
+    for(int t=0; t<1000;t++){
+        int work=0;
+    for(int i = 0; i < n; i++) {
+        work += Workload(t,temp[i][0],temp[i][1]);       
+    }
+    ans = max(work, ans);
+    }
+    cout << ans;
+    
+    return 0;
+}
