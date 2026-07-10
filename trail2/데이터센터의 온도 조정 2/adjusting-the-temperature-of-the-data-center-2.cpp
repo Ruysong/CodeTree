1b10ff2ade6d119e2f766f294ba71e3e331000a6
Ruysong <rruysong@gmail.com>
2026-07-10 19:57:01 +0900

    [Compile Error] 데이터센터의 온도 조정 2

--- trail2/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
+++ trail2/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -7,7 +7,7 @@ int n,c,g,h;
 int temp[1000][2]={};
 
 int Workload(int num, int a,int b) {
-    if(num ==0) return c;
+    
     else if(num < a)
         return c;
     else if(num <= b)
@@ -21,7 +21,7 @@ int main() {
     for(int i = 0; i < n; i++) cin >> temp[i][0]>> temp[i][1];
 
     int ans=0;
-    for(int t=0; t<=1001;t++){
+    for(int t=-1; t<=1001;t++){
         int work=0;
     for(int i = 0; i < n; i++) {
         work += Workload(t,temp[i][0],temp[i][1]);       
