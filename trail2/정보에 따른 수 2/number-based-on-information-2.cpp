7f52575d7d30a9e3bad6a81651fa62048b6ebb3a
Ruysong <rruysong@gmail.com>
2026-07-10 20:44:56 +0900

    [Wrong Answer] 정보에 따른 수 2 (2ms, 0MB)

---
+++ trail2/정보에 따른 수 2/number-based-on-information-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int t,a,b;
+
+int main() {
+    cin >> t>> a>>b;
+    char pos[1004]={};
+    int ans=0;
+
+    for(int i=0;i<t;i++){
+        char c;
+        int p;
+        cin>> c>>p;
+        pos[p] = c;
+    }
+
+     for(int i = a; i <= b; i++) {
+    int s_dist = INT_MAX;
+    int n_dist = INT_MAX;
+
+    for(int j = 0; j < t; j++) {
+        int dist = abs(i - pos[j]);
+
+        if(pos[j] == 'S') {
+            s_dist = min(s_dist, dist);
+        }
+        else if(pos[j] == 'N') {
+            n_dist = min(n_dist, dist);
+        }
+    }
+
+    if(s_dist <= n_dist) ans++;
+}
+        cout <<ans;
+
+    return 0;
+}
