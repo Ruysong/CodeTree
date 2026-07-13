9380e7728ff8099e7f2e10d4fbaf41e46e3f70c8
Ruysong <rruysong@gmail.com>
2026-07-13 14:33:41 +0900

    [Runtime Error] 트로미노 (7ms, 1MB)

--- trail4/트로미노/tromino.cpp
+++ trail4/트로미노/tromino.cpp
@@ -25,9 +25,9 @@ for(int i = 0; i < n; i++) { // 1*3짜리 먼저 진행
         ans = max(ans,sum);
     }
 }
-for(int i = 0; i < n; i++) { // 3*1 짜리 살짜쿵 돌려서
+for(int i = 0; i < n-2; i++) { // 3*1 짜리 살짜쿵 돌려서
         int sum =0;
-      for(int j = 0; j < m-2; j++) {
+      for(int j = 0; j < m; j++) {
         sum = segments[i][j]+ segments[i+1][j]+segments[i+2][j];
         ans = max(ans,sum);
     }
@@ -37,7 +37,7 @@ for(int i = 0; i < n-1; i++) { // ㄴ모양 진행.
       for(int j = 0; j < m-1; j++) {
         for(int k=i;k<i+2;k++)
             for(int l=j;l<j+2;l++){
-        sum = segments[i][j]+ segments[i+1][j]+segments[i+1][j+1]+segments[i+1][j+1];
+        sum = segments[i][j]+ segments[i+1][j]+segments[i][j+1]+segments[i+1][j+1];
         sum -= segments[k][l];
         ans = max(ans,sum);
         }
