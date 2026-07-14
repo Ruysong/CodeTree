78866c9a982cd41c854d6de3c7342471fc748338
Ruysong <rruysong@gmail.com>
2026-07-14 13:44:20 +0900

    [Wrong Answer] 야바위 (2ms, 0MB)

--- trail2/야바위/ya-rock.cpp
+++ trail2/야바위/ya-rock.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 int n;
 int max_cnt = 0;
+int pick[100][3]={};
 
 int main() {
     cin >>n;
     
-    int max_cnt =0;
+    for(int j=0;j<n; j++) cin>> pick[j][0]>>pick[j][1]>>pick[j][2];
     
     for(int i = 0; i <= 2; i++) {
         int cup[3]={0};
         cup[i]=1;
         int cnt=0;
         for(int j=0;j<n; j++){
-            int a,b,c;
-            cin >> a>> b>> c;
-            a--;b--;c--;
-            int temp = cup[a];
-            cup[a]=cup[b];
-            cup[b]=temp;
             
-            if(cup[c]==1) cnt++;
+            int temp = cup[pick[j][0]];
+            cup[pick[j][0]]=cup[pick[j][1]];
+            cup[pick[j][1]]=temp;
+            
+            if(cup[pick[j][2]]==1) cnt++;
         }
         if(cnt > max_cnt){
             max_cnt= cnt;          
