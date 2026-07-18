56f0d4625abb63cd89553b82698a49a975d0ea19
Ruysong <rruysong@gmail.com>
2026-07-18 13:29:25 +0900

    [Wrong Answer] 숫자가 더 큰 인접한 곳으로 이동 (2ms, 0MB)

---
+++ trail4/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int grid[101][101]={};
+int r,c;
+
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,-1,1};
+
+int InRange(int x,int y){
+    return x>=0 && x< n && y>=0 && y <n;
+}
+
+int main() {
+    cin >> n>> r>>c;
+    int x,y;
+    x = r-1;
+    y = c-1;
+    vector<int> v;
+
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++)
+        cin >> grid[i][j];
+    }
+
+
+    v.push_back(grid[x][y]);
+    int ok =true;
+
+    while(ok){
+        int close=grid[x][y];
+        int max_i=-1;
+        int tx=x;
+        int ty=y;
+        for(int i=0;i<4;i++){
+            int nx =x + dx[i];
+            int ny =y + dy[i];
+            if(grid[nx][ny] > close && InRange(nx,ny) ) {
+               max_i=i;
+               break;
+            }
+           
+    }
+         if(max_i != -1) {
+            x += dx[max_i];
+            y += dy[max_i];
+            v.push_back(grid[x][y]);
+        }   
+        else{ ok =false;}
+    }
+
+    for(int i=0;i<v.size();i++){ 
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
