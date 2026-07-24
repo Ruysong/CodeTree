8a361a3867e6c6353185f64cf6fc92899e30e48a
Ruysong <rruysong@gmail.com>
2026-07-24 15:41:11 +0900

    [Compile Error] K개 중에 1개를 N번 뽑기

---
+++ trail4/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int n,k;
+vector<int> answer;
+
+void Choose(int cnt){
+
+    if(cnt ==N)
+    {
+        //이제 자릿수 다 찼다면
+       for (int i = 0; i < answer.size(); i++) {
+    cout << answer[i] << ' ';
+}
+    }
+
+    //원래는
+    for(int i=1;i<=k;i++)
+    {
+        answer.push_back(i)
+
+        Choose(cnt+1);
+
+        answer.pop_back();
+    }
+
+
+}
+int main() {
+    cin >> n>>k;
+
+    Choose(0);
+
+    return 0;
+}
