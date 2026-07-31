02ae7f4c185c75448e423e2a18847c524ac0fd75
Ruysong <rruysong@gmail.com>
2026-07-31 20:02:06 +0900

    [Passed] 가능한 수열 중 최솟값 구하기 (3ms, 0MB)

--- trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
+++ trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -4,38 +4,41 @@ using namespace std;
 int n;
 int arr[81];
 bool found = false;
-void determine(){
+int determine(int x){
 
     //subsequence의 길이를 1부터 n/2까지 잡을 수 있겟고..
     int ok=true;
     for(int len=1;len<=n/2;len++){
-        for (int i = 0; i + 2 * len <= n; i++){
+        for (int i = 0; i + 2 * len <= x; i++){
             int cnt=0;
            for(int j=0;j<len;j++){ 
             if(i+len+j > n) continue;
             if( arr[i+j] ==arr[i+len+j]) cnt++;
            }
-           if(cnt==len) ok =false;
+           if(cnt==len) {ok =false; break;}
     }
     }
-    if (ok ==true){
+    if (ok ==true&& x==n){
         for(int i=0;i<n;i++)
         cout << arr[i];
         found = true;
-        return ;
+        return 0;
     }
+    return ok;
 }
 
 void makeS(int x){ //일단 뭐.. 수열 만듭시다. 
 
     if (found) return;
     if(x==n){ //길이 n짜리 수열 
-        determine();
+        determine(x);
         return;
     }
     for(int i=4;i<=6;i++){
         arr[x]=i;
+        if(determine(x+1))
         makeS(x+1);
+        if(found) return;
 
     }
 
